1d6d514eb2a6f85ccb69b4a3cabeb8f512d31400
Jacob Tutlis <jtutlis@gmail.com>
2019-04-15 11:30:12 -0400

    Added the layout. TODO: UI, Database, Back button

--- src/main/java/api/APIProvideController.java
+++ src/main/java/api/APIProvideController.java
@@ -2,14 +2,30 @@ package api;
 
 
 import base.EnumScreenType;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
 import controller.Controller;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class APIProvideController extends Controller implements Initializable {
+
+    @FXML
+    JFXTextArea getPatientID;
+    @FXML
+    JFXButton submit;
+    @FXML
+    JFXComboBox drugName;
+    @FXML
+    JFXTextArea drugDescription;
+
+
+
 
     @Override
     public void init(URL location, ResourceBundle resources) {
@@ -19,6 +35,17 @@ public class APIProvideController extends Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void submitButtonClick(ActionEvent actionEvent){
+        String patientID = getPatientID.getText();
+        // @Ryan Query database for all of the patients open prescriptions
+        // populate combobox with all of the different open prescriptions
+    }
+
+    public void getDrugDesciption(ActionEvent actionEvent){
+       // get drug name from combobx
+        // populate drugDescription with the drug information
     }
 
 }
